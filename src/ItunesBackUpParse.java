f82d53a08acaee06aa8f72b9b3e758f3a1534341
Conor Shields <42182485+cshields236@users.noreply.github.com>
2019-11-26 19:52:33 +0000

    parsed name and song

--- src/ItunesBackUpParse.java
+++ src/ItunesBackUpParse.java
@@ -25,18 +25,33 @@ public class ItunesBackUpParse{
 
         NodeList tracks = doc.getElementsByTagName("dict");
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tracks.getLength(); i++) {
             Node track = tracks.item(i);
             //System.out.println(track.getNodeName());
 
             NodeList ts = (NodeList) tracks.item(i);
-            for (int j = 0; j < 10; j++) {
+            for (int j = 0; j < ts.getLength(); j++) {
 
                 Node t = ts.item(j);
                 //System.out.println(t.getNodeName());
 
                 NodeList tw = (NodeList) ts.item(j);
 
+                for (int p = 0; p < tw.getLength(); p++){
+                    if(tw.item(p) != null) {
+                        Node z = tw.item(p);
+                        //System.out.println(z.getTextContent());
+                        if (tw.item(p).getTextContent().equalsIgnoreCase("Artist"))
+                        {
+                            Node artist = tw.item(p).getNextSibling();
+                           // System.out.println(artist.getTextContent());
+                        }
+                        if(tw.item(p).getTextContent().equalsIgnoreCase("Name")){
+                            Node songName = tw.item(p).getNextSibling();
+                            System.out.println(songName.getTextContent());
+                        }
+                    }
+                }
 
                 //Node y = tw.item(0);
 
